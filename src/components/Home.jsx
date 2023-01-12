a39dde997c20d57ff89abb19372dc3d82e626a56
Ryan Reeves <reeves0ryan@gmail.com>
2023-01-12 12:36:52 -0800

    Update project images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,63 +1,90 @@
 import React from 'react';
-import homePortrait from "../assets/self.png";
-import linkedinIcon from "../assets/icon-images/linkedin-icon.png";
-import githubIcon from "../assets/icon-images/github-icon.png";
-import instagramIcon from "../assets/icon-images/instagram-icon.png";
-import mountainsImage from "../assets/icon-images/mountains.png";
-import treesImage from "../assets/trees.png";
-import city1Image from "../assets/city1.png";
-import city2Image from "../assets/city2.png";
+import homePortrait from '../assets/self.png';
+import linkedinIcon from '../assets/icon-images/linkedin-icon.png';
+import githubIcon from '../assets/icon-images/github-icon.png';
+import instagramIcon from '../assets/icon-images/instagram-icon.png';
+import mountainsImage from '../assets/bg-images/mountains.png';
+import treesImage from '../assets/bg-images/trees.png';
+import city1Image from '../assets/bg-images/city1.png';
+import city2Image from '../assets/bg-images/city2.png';
 
 import '../assets/style/home.css';
 
 export default function Home({ handlePageChange }) {
-    return (
-        <div className="home-container">
-            <div className="home-background">
-                <img src={mountainsImage} alt="mountains" className="home-mountains" />
-                <img src={treesImage} alt="trees" className="home-trees" />
-                <img src={city1Image} alt="city1" className="home-city1" />
-                <img src={city2Image} alt="city2" className="home-city2" />
-            </div>
-            <div className="home-hub">
-                <div className="social">
-                    <img src={homePortrait} alt="home" className="home-picture" />
+	return (
+		<div className='home-container'>
+			<div className='home-background'>
+				<img src={mountainsImage} alt='mountains' className='home-mountains' />
+				<img src={treesImage} alt='trees' className='home-trees' />
+				<img src={city1Image} alt='city1' className='home-city1' />
+				<img src={city2Image} alt='city2' className='home-city2' />
+			</div>
+			<div className='home-hub'>
+				<div className='social'>
+					<img src={homePortrait} alt='home' className='home-picture' />
 
-                    <div className="social-buttons">
-                            <a href="https://www.linkedin.com/in/rreevesdev/">
-                                <img src={linkedinIcon} alt="linkedin" id="linkedin-icon" className="base-icon" />
-                            </a>
-                            <a href="https://github.com/rreeves1996">
-                                <img src={githubIcon} alt="github" id="github-icon" className="base-icon" />
-                            </a>
-                            <a href="https://www.instagram.com/ryanmakesloudnoises/">
-                                <img src={instagramIcon} alt="instagram" id="instagram-icon" className="base-icon" />
-                            </a>
-                    </div>
-                    <div className="home-nav">
-                        <div className="home-nav-links">
-                            <a href="#about" 
-                                onClick={() => handlePageChange("About Me")}
-                                id="home-about">› About</a>
-                            <a href="#portfolio" 
-                                onClick={() => handlePageChange("Portfolio")}
-                                id="home-projects">› Projects</a>
-                            <a href="#resume"
-                                onClick={() => handlePageChange("Resume")}
-                                id="home-resume">› Resume</a>
-                            <a href="#contact"
-                                onClick={() => handlePageChange("Contact")}
-                                id="home-contact">› Contact</a>
-                        </div>
-                    </div>
-                </div>
+					<div className='social-buttons'>
+						<a href='https://www.linkedin.com/in/rreevesdev/'>
+							<img
+								src={linkedinIcon}
+								alt='linkedin'
+								id='linkedin-icon'
+								className='base-icon'
+							/>
+						</a>
+						<a href='https://github.com/rreeves1996'>
+							<img
+								src={githubIcon}
+								alt='github'
+								id='github-icon'
+								className='base-icon'
+							/>
+						</a>
+						<a href='https://www.instagram.com/ryanmakesloudnoises/'>
+							<img
+								src={instagramIcon}
+								alt='instagram'
+								id='instagram-icon'
+								className='base-icon'
+							/>
+						</a>
+					</div>
+					<div className='home-nav'>
+						<div className='home-nav-links'>
+							<a
+								href='#about'
+								onClick={() => handlePageChange('About Me')}
+								id='home-about'>
+								› About
+							</a>
+							<a
+								href='#portfolio'
+								onClick={() => handlePageChange('Portfolio')}
+								id='home-projects'>
+								› Projects
+							</a>
+							<a
+								href='#resume'
+								onClick={() => handlePageChange('Resume')}
+								id='home-resume'>
+								› Resume
+							</a>
+							<a
+								href='#contact'
+								onClick={() => handlePageChange('Contact')}
+								id='home-contact'>
+								› Contact
+							</a>
+						</div>
+					</div>
+				</div>
 
-                <header className="hub-label">
-                    <h6>Hi there, I'm</h6>
-                    <h1>Ryan Reeves</h1>
-                    <h4>designer, developer, engineer</h4>
-                </header>
-            </div>
-        </div>
-    )
+				<header className='hub-label'>
+					<h6>Hi there, I'm</h6>
+					<h1>Ryan Reeves</h1>
+					<h4>designer, developer, engineer</h4>
+				</header>
+			</div>
+		</div>
+	);
 }
--- src/components/subs/Carousel.jsx
+++ src/components/subs/Carousel.jsx
@@ -1,206 +1,234 @@
-import React from "react";
-import Project from "./Project";
-import soloProject1 from "../../assets/proj-images/ecommerce.png";
-import soloProject2 from "../../assets/proj-images/emptracker.png";
-import soloProject3 from "../../assets/proj-images/readmegen.png";
-import soloProject4 from "../../assets/proj-images/teamgen.png";
-import soloProject5 from "../../assets/proj-images/weather.png";
-import groupProject1 from "../../assets/proj-images/ftf.png";
-import groupProject2 from "../../assets/proj-images/smp.png";
-import groupProject3 from "../../assets/proj-images/placeholder.png";
+import React from 'react';
+import Project from './Project';
+import soloProject1 from '../../assets/proj-images/tech-blog.png';
+import soloProject2 from '../../assets/proj-images/weather.png';
+import soloProject3 from '../../assets/proj-images/teamez.png';
+import soloProject4 from '../../assets/proj-images/todo.png';
+import soloProject5 from '../../assets/proj-images/keyboard.png';
+import groupProject1 from '../../assets/proj-images/ftf.png';
+import groupProject2 from '../../assets/proj-images/smp.png';
+import groupProject3 from '../../assets/proj-images/placeholder.png';
 
 export default function Carousel(props) {
-    const nextButton = `❭`;
-    const prevButton = `❬`;
-    const soloProjects = [
-        {
-            github: "https://github.com/rreeves1996/week13-hw",
-            img: soloProject1,
-            title: "E-Commerce Back-End",
-            text: "Express, MySQL, ORM"
-        },
-        {
-            github: "https://github.com/rreeves1996/week12-hw",
-            img: soloProject2,
-            title: "HR Employee Tracker",
-            text: "Node, MySQL, Inquirer"
-        },
-        {
-            github: "https://github.com/rreeves1996/week9-hw",
-            img: soloProject3,
-            title: "README Generator",
-            text: "Node, Command line, Inquirer"
-        },
-        {
-            github: "https://github.com/rreeves1996/week10-hw",
-            img: soloProject4,
-            title: "Team Profile Generator",
-            text: "Node, Command line, Inquirer"
-        },
-        {
-            github: "https://github.com/rreeves1996/week6-hw",
-            img: soloProject5,
-            title: "Weather Dashboard",
-            text: "Third-Party API, JavaScript"
-        }]
-    const groupProjects = [
-        {
-            github: "https://the-randumbizer.herokuapp.com/",
-            img: groupProject3,
-            title: "The RanDUMBizer",
-            repo: "https://github.com/EyesackEl/the-ranDUMBizer"
-        },
-        {
-            github: "https://eyesackel.github.io/fork-the-food/",
-            img: groupProject1,
-            title: "Fork The Food",
-            repo: "https://github.com/EyesackEl/fork-the-food"
-        },
-        {
-            github: "https://stack-my-paddle.herokuapp.com/",
-            img: groupProject2,
-            title: "Stack My Paddle",
-            repo: "https://github.com/furhan-dev/pickleball"
-        },
-        {
-            github: "https://the-randumbizer.herokuapp.com/",
-            img: groupProject3,
-            title: "The RanDUMBizer",
-            repo: "https://github.com/EyesackEl/the-ranDUMBizer"
-        },
-        {
-            github: "https://eyesackel.github.io/fork-the-food/",
-            img: groupProject1,
-            title: "Fork The Food",
-            repo: "https://github.com/EyesackEl/fork-the-food"
-        },
-        {
-            github: "https://stack-my-paddle.herokuapp.com/",
-            img: groupProject2,
-            title: "Stack My Paddle",
-            repo: "https://github.com/furhan-dev/pickleball"
-        }]
+	const nextButton = `❭`;
+	const prevButton = `❬`;
+	const soloProjects = [
+		{
+			github: 'https://rreeves-tech-blog.herokuapp.com/',
+			img: soloProject1,
+			title: 'Tech Blog',
+			text: 'Handlebars, MySQL, Node',
+		},
+		{
+			github: 'https://github.com/rreeves1996/week6-hw',
+			img: soloProject2,
+			title: 'Weather Dashboard',
+			text: 'Third-Party API, JavaScript',
+		},
+		{
+			github: 'https://teamez.herokuapp.com/',
+			img: soloProject3,
+			title: 'TeamEZ - Team Manager',
+			text: 'React, Redux, MySQL, Node',
+		},
+		{
+			github: 'https://rreeves-todo-list.herokuapp.com/',
+			img: soloProject4,
+			title: 'To-Do List',
+			text: 'React, Bootstrap, Node',
+		},
+		{
+			github: 'https://rreeves-virtual-keyboard.herokuapp.com/',
+			img: soloProject5,
+			title: 'Virtual Keyboard',
+			text: 'React, Node',
+		},
+	];
+	const groupProjects = [
+		{
+			github: 'https://the-randumbizer.herokuapp.com/',
+			img: groupProject3,
+			title: 'The RanDUMBizer',
+			repo: 'https://github.com/EyesackEl/the-ranDUMBizer',
+		},
+		{
+			github: 'https://eyesackel.github.io/fork-the-food/',
+			img: groupProject1,
+			title: 'Fork The Food',
+			repo: 'https://github.com/EyesackEl/fork-the-food',
+		},
+		{
+			github: 'https://stack-my-paddle.herokuapp.com/',
+			img: groupProject2,
+			title: 'Stack My Paddle',
+			repo: 'https://github.com/furhan-dev/pickleball',
+		},
+		{
+			github: 'https://the-randumbizer.herokuapp.com/',
+			img: groupProject3,
+			title: 'The RanDUMBizer',
+			repo: 'https://github.com/EyesackEl/the-ranDUMBizer',
+		},
+		{
+			github: 'https://eyesackel.github.io/fork-the-food/',
+			img: groupProject1,
+			title: 'Fork The Food',
+			repo: 'https://github.com/EyesackEl/fork-the-food',
+		},
+		{
+			github: 'https://stack-my-paddle.herokuapp.com/',
+			img: groupProject2,
+			title: 'Stack My Paddle',
+			repo: 'https://github.com/furhan-dev/pickleball',
+		},
+	];
 
-    return (
-        <>
-            <div className="slider">                  
-                <div className="slider-bg"></div>
-                {props.side === "Solo" ? (
-                    <>
-                        {soloProjects.map((project, index) => (
-                            <>
-                                <Project side="Solo" id={index} github={project.github} img={project.img} title={project.title} text={project.text} />
-                            </>
-                        ))}
+	return (
+		<>
+			<div className='slider'>
+				<div className='slider-bg'></div>
+				{props.side === 'Solo' ? (
+					<>
+						{soloProjects.map((project, index) => (
+							<>
+								<Project
+									side='Solo'
+									id={index}
+									github={project.github}
+									img={project.img}
+									title={project.title}
+									text={project.text}
+								/>
+							</>
+						))}
 
-                        <button className="next-btn"
-                            onClick={() => {
-                                let soloSlides = document.querySelectorAll(".solo-slide");
-                                
-                                soloSlides.forEach(slide => {
-                                    let slideDataID = slide.getAttribute('data-id');
-                                    let slidePos = slideDataID - 2;
-                                    
-                                    slidePos--;
-                                    if(slidePos < -2) {
-                                        slide.style.transform = `translateX(${(slidePos * 100) + 500}%)`;
-                                        slide.setAttribute("data-id", 4);
-                                        slide.style.zIndex = 0;
-                                        console.log(slidePos);
-                                    } else {
-                                        slide.style.transform = `translateX(${slidePos * 100}%)`;
-                                        slide.setAttribute("data-id", (slidePos + 2));
-                                        slide.style.zIndex = 2;
-                                        console.log(slidePos);
-                                    }
-                                });
-                            }}>
-                            <div className="arrow">{nextButton}</div>
-                        </button>
-                        <button className="prev-btn"
-                            onClick={() => {
-                                let soloSlides = document.querySelectorAll(".solo-slide");
-                                
-                                soloSlides.forEach(slide => {
-                                    let slideDataID = slide.getAttribute('data-id');
-                                    let slidePos = slideDataID - 2;
-                                    
-                                    slidePos++;
-                                    if(slidePos > 2) {
-                                        slide.style.transform = `translateX(${(slidePos * 100) - 500}%)`;
-                                        slide.setAttribute("data-id", 0);
-                                        slide.style.zIndex = 0;
-                                        console.log(slidePos);
-                                    } else {
-                                        slide.style.transform = `translateX(${slidePos * 100}%)`;
-                                        slide.setAttribute("data-id", (slidePos + 2));
-                                        slide.style.zIndex = 2;
-                                        console.log(slidePos);
-                                    }
-                                });
-                            }}>
-                            <div className="arrow">{prevButton}</div>
-                        </button>
-                    </>
-                ) : (
-                    <>
-                        {groupProjects.map((project, index) => (
-                            <>
-                                <Project side="Group" id={index + 5} github={project.github} img={project.img} title={project.title} text={project.repo} />
-                            </>
-                        ))}
+						<button
+							className='next-btn'
+							onClick={() => {
+								let soloSlides = document.querySelectorAll('.solo-slide');
 
-                        <button className="next-btn"
-                            onClick={() => {
-                                let groupSlides = document.querySelectorAll(".group-slide");
-                                
-                                groupSlides.forEach(slide => {
-                                    let slideDataID = slide.getAttribute('data-id');
-                                    let slidePos = slideDataID - 7;
-                                    
-                                    slidePos--;
-                                    if(slidePos < -2) {
-                                        slide.style.transform = `translateX(${(slidePos * 100) + 600}%)`;
-                                        slide.setAttribute("data-id", 10);
-                                        slide.style.zIndex = 0;
-                                        console.log(slidePos);
-                                    } else {
-                                        slide.style.transform = `translateX(${slidePos * 100}%)`;
-                                        slide.setAttribute("data-id", (slidePos + 7));
-                                        slide.style.zIndex = 2;
-                                        console.log(slidePos);
-                                    }
-                                });
-                            }}>
-                            <div className="arrow">{nextButton}</div>
-                        </button>
-                        <button className="prev-btn"
-                            onClick={() => {
-                                let groupSlides = document.querySelectorAll(".group-slide");
-                                
-                                groupSlides.forEach(slide => {
-                                    let slideDataID = slide.getAttribute('data-id');
-                                    let slidePos = slideDataID - 7;
-                                    
-                                    slidePos++;
-                                    if(slidePos > 3) {
-                                        slide.style.transform = `translateX(${(slidePos * 100) - 600}%)`;
-                                        slide.setAttribute("data-id", 5);
-                                        slide.style.zIndex = 0;
-                                        console.log(slidePos);
-                                    } else {
-                                        slide.style.transform = `translateX(${slidePos * 100}%)`;
-                                        slide.setAttribute("data-id", (slidePos + 7));
-                                        slide.style.zIndex = 2;
-                                        console.log(slidePos);
-                                    }
-                                });
-                            }}>
-                            <div className="arrow">{prevButton}</div>
-                        </button>
-                    </>
-                )}
-            </div>
-        </>
-    )
+								soloSlides.forEach((slide) => {
+									let slideDataID = slide.getAttribute('data-id');
+									let slidePos = slideDataID - 2;
+
+									slidePos--;
+									if (slidePos < -2) {
+										slide.style.transform = `translateX(${
+											slidePos * 100 + 500
+										}%)`;
+										slide.setAttribute('data-id', 4);
+										slide.style.zIndex = 0;
+										console.log(slidePos);
+									} else {
+										slide.style.transform = `translateX(${slidePos * 100}%)`;
+										slide.setAttribute('data-id', slidePos + 2);
+										slide.style.zIndex = 2;
+										console.log(slidePos);
+									}
+								});
+							}}>
+							<div className='arrow'>{nextButton}</div>
+						</button>
+						<button
+							className='prev-btn'
+							onClick={() => {
+								let soloSlides = document.querySelectorAll('.solo-slide');
+
+								soloSlides.forEach((slide) => {
+									let slideDataID = slide.getAttribute('data-id');
+									let slidePos = slideDataID - 2;
+
+									slidePos++;
+									if (slidePos > 2) {
+										slide.style.transform = `translateX(${
+											slidePos * 100 - 500
+										}%)`;
+										slide.setAttribute('data-id', 0);
+										slide.style.zIndex = 0;
+										console.log(slidePos);
+									} else {
+										slide.style.transform = `translateX(${slidePos * 100}%)`;
+										slide.setAttribute('data-id', slidePos + 2);
+										slide.style.zIndex = 2;
+										console.log(slidePos);
+									}
+								});
+							}}>
+							<div className='arrow'>{prevButton}</div>
+						</button>
+					</>
+				) : (
+					<>
+						{groupProjects.map((project, index) => (
+							<>
+								<Project
+									side='Group'
+									id={index + 5}
+									github={project.github}
+									img={project.img}
+									title={project.title}
+									text={project.repo}
+								/>
+							</>
+						))}
+
+						<button
+							className='next-btn'
+							onClick={() => {
+								let groupSlides = document.querySelectorAll('.group-slide');
+
+								groupSlides.forEach((slide) => {
+									let slideDataID = slide.getAttribute('data-id');
+									let slidePos = slideDataID - 7;
+
+									slidePos--;
+									if (slidePos < -2) {
+										slide.style.transform = `translateX(${
+											slidePos * 100 + 600
+										}%)`;
+										slide.setAttribute('data-id', 10);
+										slide.style.zIndex = 0;
+										console.log(slidePos);
+									} else {
+										slide.style.transform = `translateX(${slidePos * 100}%)`;
+										slide.setAttribute('data-id', slidePos + 7);
+										slide.style.zIndex = 2;
+										console.log(slidePos);
+									}
+								});
+							}}>
+							<div className='arrow'>{nextButton}</div>
+						</button>
+						<button
+							className='prev-btn'
+							onClick={() => {
+								let groupSlides = document.querySelectorAll('.group-slide');
+
+								groupSlides.forEach((slide) => {
+									let slideDataID = slide.getAttribute('data-id');
+									let slidePos = slideDataID - 7;
+
+									slidePos++;
+									if (slidePos > 3) {
+										slide.style.transform = `translateX(${
+											slidePos * 100 - 600
+										}%)`;
+										slide.setAttribute('data-id', 5);
+										slide.style.zIndex = 0;
+										console.log(slidePos);
+									} else {
+										slide.style.transform = `translateX(${slidePos * 100}%)`;
+										slide.setAttribute('data-id', slidePos + 7);
+										slide.style.zIndex = 2;
+										console.log(slidePos);
+									}
+								});
+							}}>
+							<div className='arrow'>{prevButton}</div>
+						</button>
+					</>
+				)}
+			</div>
+		</>
+	);
 }
